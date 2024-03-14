ab4391a3880ecaa137f551e35b8d452e5ec2bd13
Ronan Avila <avila.ronan@gmail.com>
2024-03-14 07:09:44 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { Post } from "./Post.jsx";
 import { Header } from "./components/Header.jsx";
-import "./styles.css";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar.jsx";
+
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Batman da Silva"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, praesentium magnam. Minima laborum doloribus maiores illo voluptatibus animi illum quasi quis expedita ut, cumque nemo nihil soluta, laudantium, obcaecati voluptatum?"
-      />
-      <Post
-        author="Robin Fernandes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, praesentium magnam. Minima laborum doloribus maiores illo voluptatibus animi illum quasi quis expedita ut, cumque nemo nihil soluta, laudantium, obcaecati voluptatum?"
-      />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Batman da Silva"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, praesentium magnam. Minima laborum doloribus maiores illo voluptatibus animi illum quasi quis expedita ut, cumque nemo nihil soluta, laudantium, obcaecati voluptatum?"
+          />
+          <Post
+            author="Robin Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, praesentium magnam. Minima laborum doloribus maiores illo voluptatibus animi illum quasi quis expedita ut, cumque nemo nihil soluta, laudantium, obcaecati voluptatum?"
+          />
+        </main>
+      </div>
     </>
   );
 }
